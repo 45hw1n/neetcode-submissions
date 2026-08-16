46e8e23fc0154749ebd5b6f28804c550fcc7992d
45hw1n <64681289+45hw1n@users.noreply.github.com>
2026-08-16 13:07:37 +0530

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        const result = new Array(nums.length).fill(1);
+
+        // Pass 1: store product of everything to the LEFT
+        let leftProduct = 1;
+
+        for (let i = 0; i < nums.length; i++) {
+            result[i] = leftProduct;
+            leftProduct *= nums[i];
+        }
+
+        // Pass 2: multiply product of everything to the RIGHT
+        let rightProduct = 1;
+
+        for (let i = nums.length - 1; i >= 0; i--) {
+            result[i] *= rightProduct;
+            rightProduct *= nums[i];
+        }
+
+        return result;
+    }
+}
